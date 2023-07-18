a6e241453452633e8c4b45d58125cd9660ae36d1
Daniel Castro de Farias <45545000+danielcastrof@users.noreply.github.com>
2023-07-18 19:39:51 -0300

    adicionando swagger

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,25 @@
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const app = await NestFactory.create(AppModule, { cors: true });
+  app.enableCors();
+
+
+  const config = new DocumentBuilder()
+    .setTitle('Documentação com Swagger - Order Food API')
+    .setDescription(
+      'Projeto back-end DEV WEB.',
+    )
+    .setVersion('1.0')
+    .addTag('Usuários')
+    .addBearerAuth()
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('swagger', app, document);
+
+  await app.listen(8000);
 }
 bootstrap();
